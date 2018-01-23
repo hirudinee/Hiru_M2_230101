e08455c7e30a22d31aa99c948b94d3f1f82a95f8
hirudinee <35219641+hirudinee@users.noreply.github.com>
2018-01-23 11:51:45 +0530

    SNS get toppic attributes

--- Hiru_M2_230101/Hiru_M2_230101-lambda.js
+++ Hiru_M2_230101/Hiru_M2_230101-lambda.js
@@ -2,17 +2,20 @@ let AWS = require('aws-sdk');
 const sns = new AWS.SNS();
 exports.handler = function (event, context, callback) {
 
-	sns.subscribe({
-		Protocol: 'email',
-		Endpoint: 'hirudineel@gmail.com',
+
+	sns.getTopicAttributes({
 		TopicArn: 'arn:aws:sns:us-east-1:480964559519:hiru_topic'
 	}).promise()
 		.then(data => {
 			// your code goes here
+
+			callback(null, data);
 		})
 		.catch(err => {
 			// error handling goes here
+			callback(null, err);
 		});
+
 
 	callback(null, 'Successfully executed');
 }
